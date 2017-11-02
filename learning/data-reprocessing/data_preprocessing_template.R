ada761a912b573d6ed96af28457b4e50fd50bac2
Yulong Tan <yulongtan67@gmail.com>
2017-11-02 14:41:10 -0700

    reads data

--- learning/data-reprocessing/data_preprocessing_template.R
+++ learning/data-reprocessing/data_preprocessing_template.R
@@ -2,5 +2,7 @@
 # 10.31.17
 
 # Using libraries that are installed
-# install.package('dplyr') <- this is only done once per machine, and can be done within the console
-library(dplyr) # using the library dplyr
+# install.package('packagename') <- this is only done once per machine, and can be done within the console
+
+# Importing the dataset
+dataset <- read.csv('Data.csv')
--- learning/data-reprocessing/data_preprocessing_template.py
+++ learning/data-reprocessing/data_preprocessing_template.py
@@ -7,9 +7,16 @@ Yulong Tan
 """
 
 # Importing the libraries
-
 import numpy as np
 import matplotlib.pyplot as plot
 import pandas as pd
 
+# Importing the datasets
+dataset = pd.read_csv('Data.csv')
 
+# Preparing the data
+# We need to separate the independent (x) variables from the dependent (y) ones
+# Rows are on the left of the comma, and cols are on the right
+# : Means range. num:num
+x = dataset.iloc[:, :-1].values # We want all the rows, and every column exept the last one
+y = dataset.iloc[:, 3].values # We want all the tows, but only the 3rd column
