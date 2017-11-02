284109d6acb1308568e4b764e857746d0afffeab
Yulong Tan <yulongtan67@gmail.com>
2017-11-02 15:33:34 -0700

    adds code to deal with missing data

--- learning/data-reprocessing/data_preprocessing_template.R
+++ learning/data-reprocessing/data_preprocessing_template.R
@@ -6,3 +6,16 @@
 
 # Importing the dataset
 dataset <- read.csv('Data.csv')
+
+# Taking care of missing data
+ReplaceColWithAverage <- function(col) {
+  # ifelse is basically a ternary operator
+  new.col <- ifelse(is.na(col),
+                    ave(col, FUN = function(x) mean(x, na.rm = TRUE)),
+                    col)
+  return(new.col)
+}
+
+# $ symbol grabs the column by name
+dataset$Age <- ReplaceColWithAverage(dataset$Age)
+dataset$Salary <- ReplaceColWithAverage(dataset$Salary)
--- learning/data-reprocessing/data_preprocessing_template.py
+++ learning/data-reprocessing/data_preprocessing_template.py
@@ -17,6 +17,12 @@ dataset = pd.read_csv('Data.csv')
 # Preparing the data
 # We need to separate the independent (x) variables from the dependent (y) ones
 # Rows are on the left of the comma, and cols are on the right
-# : Means range. num:num
+# : Means range. num:num (upper bound is excluded)
 x = dataset.iloc[:, :-1].values # We want all the rows, and every column exept the last one
 y = dataset.iloc[:, 3].values # We want all the tows, but only the 3rd column
+
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0) # looking for values that are NaN and replacing them with the mean
+imputer = imputer.fit(x[:, 1:3]) # fixing all rows of cols 1 and 2
+x[:, 1:3] = imputer.transform(x[:, 1:3]) # set the x data to the fixed table
